583ed180a7dc5ab53939e0f3e92d86abb7036991
Karen Avagyan <karen@ausdia.com>
2023-03-23 14:24:08 +0000

    reformatted

--- src/table.cpp
+++ src/table.cpp
@@ -3,29 +3,33 @@
 
 namespace my_namespace {
 
-static std::string getName(const myNode *n) {
+static std::string getName(const myNode* n) {
   return n ? n->getName() : std::string("(nil)");
 }
 
 Table::~Table() {
-  BOOST_FOREACH (TableInst *tableInst, tables) {
-    if (tableInst)
+  BOOST_FOREACH (TableInst* tableInst, tables) {
+    if (tableInst) {
       delete tableInst;
+    }
   }
   END_BOOST_FOREACH;
 }
 
-bool Table::add(
-    const myNode *node, const myNode *nodePointer, const Value &value,
-    unsigned int source,
-    bool sadPrecedence, // set_precedence value for checking in whole hierachy
-    int level) {
+bool Table::add(const myNode* node,
+                const myNode* nodePointer,
+                const Value&  value,
+                unsigned int  source,
+                bool sadPrecedence, // set_precedence value for checking in
+                                    // whole hierachy
+                int level) {
   if (!done) {
     if (!inValid) {
       assert(false);
 
-      if (instance().getAttr("err"))
+      if (instance().getAttr("err")) {
         std::cout("ERR_001", __LINE__);
+      }
       return false;
     }
     initialize(level);
@@ -34,31 +38,33 @@ bool Table::add(
   unsigned int index = node->getParent();
   if (index < tables.size()) {
     if (tables[index]) {
-      return tables[index]->set(nodePointer, node, value, value, source,
-                                sadPrecedence, level);
+      return tables[index]->set(
+          nodePointer, node, value, value, source, sadPrecedence, level);
     }
   }
 
   if (instance().getAttr("err")) {
 
-    const myList *context = _db->getlist();
+    const myList* context = _db->getlist();
 
-    const myInst *inst = node->getInst();
+    const myInst* inst = node->getInst();
 
-    std::cout("NOTE_00014", inst->getName(context), index, tables.size(),
-              __LINE__);
+    std::cout(
+        "NOTE_00014", inst->getName(context), index, tables.size(), __LINE__);
   }
   return !value.isNull();
 }
 
 void Table::advance(int count) {
   if (count > 0) {
-    while (count-- > 0)
+    while (count-- > 0) {
       ++_iter;
+    }
 
   } else if (count < 0) {
-    while (count++ < 0)
+    while (count++ < 0) {
       --_iter;
+    }
   }
 }
 
